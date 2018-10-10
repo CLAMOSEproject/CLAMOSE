90daa50f591a05d3efb9e9573c6ee6d71cfc0905
Jastin-Infj <takumiyokoo@gmail.com>
2018-10-10 12:31:54 +0900

    ボタン入力後の処理を実装しました。

---
+++ CLAMOSE/Assets/Script/ChargeMachine/GameManager/BatteryCharge.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BatteryCharge : MonoBehaviour
+{
+    private int  batteryRate;
+    private int  buttoninputCount;
+    private bool winFlag;
+
+    //チャージ倍率
+    public int chargeMagnification;
+
+    //何カウントで1%にするのか？
+    public int OneCountfromRate;
+
+    // Use this for initialization
+    void Start()
+    {
+        this.batteryRate = 0;
+        this.buttoninputCount = 0;
+        this.winFlag = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.ChargeMax())
+        {
+            this.winFlag = true;
+            Debug.Log("勝者　" + this.WinnerPlayer());
+        }
+
+        if (this.ButtonInputCheck())
+        {
+            this.buttoninputCount += this.ButtonInputCount();
+            this.CountfromRate();
+        }
+        Debug.Log(this.batteryRate + "％");
+    }
+
+    //ボタンの判定
+    bool ButtonInputCheck()
+    {
+        return Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.B);
+    }
+
+    int ButtonInputCount()
+    {
+        return 1 * this.chargeMagnification;
+    }
+
+    bool ChargeMax()
+    {
+        return this.batteryRate == 100; 
+    }
+
+    public int getbatteryRate()
+    {
+        return this.batteryRate;
+    }
+
+    public string WinnerPlayer()
+    {
+        return this.gameObject.name;
+    }
+
+    void CountfromRate()
+    {
+        this.batteryRate = this.buttoninputCount / this.OneCountfromRate;
+    }
+}
---
+++ CLAMOSE/Assets/Script/ChargeMachine/GameManager/OverCharge.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OverCharge : MonoBehaviour {
+
+    private BatteryCharge batteryChargeRate;
+
+    private enum State
+    {
+        Normal,
+        OverChargeCount,
+        OverCharge
+    }
+
+    State overchargeState;
+
+    public  int  overchargelimitTime;
+    private int  overchargeinitiallimitTime;
+    float        overchargetimeCount;
+
+    private bool overchargeFlag;
+
+	// Use this for initialization
+	void Start ()
+    {
+        this.overchargeinitiallimitTime = this.overchargelimitTime;
+        this.overchargeFlag = false;
+        this.batteryChargeRate = GetComponent<BatteryCharge>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        State chengeState = this.overchargeState;
+
+        switch (this.overchargeState)
+        {
+            case State.Normal:
+                if (this.Check())
+                {
+                    chengeState = State.OverChargeCount;
+                }
+                break;
+            case State.OverChargeCount:
+                if(!this.Check())
+                {
+                    this.LimitReset();
+                    chengeState = State.Normal;
+                }
+                if (this.overchargelimitTime <= 0)
+                {
+                    chengeState = State.OverCharge;
+                    Debug.Log("爆発");
+                }
+                this.overchargetimeCount += Time.deltaTime;
+                if(this.overchargetimeCount >= 1.0f)
+                {
+                    this.overchargetimeCount = 0;
+                    this.overchargelimitTime -= 1; 
+                }
+                Debug.Log(this.overchargelimitTime);
+                break;
+            case State.OverCharge:
+                this.LimitReset();
+                this.overchargeFlag = true;
+                //エフェクトへ続く
+                break;
+        }
+
+        this.overchargeState = chengeState;
+	}
+
+    bool Check()
+    {
+        return this.batteryChargeRate.getbatteryRate() > 100;
+    }
+
+    public bool getOverChargeFlag()
+    {
+        return this.overchargeFlag;
+    }
+
+    void LimitReset()
+    {
+        this.overchargetimeCount = 0.0f;
+        this.overchargelimitTime = this.overchargeinitiallimitTime;
+    }
+}
