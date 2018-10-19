9b6bbfc759e82ae0be19b5236440a9d28d1a6e48
Jastin <takumiyokoo@gmail.com>
2018-10-19 16:54:55 +0900

    バッテリー率のチャージを実装(少し修正が必要です)

--- CLAMOSE/Assets/Script/ChargeMachine/Text/BatteryColor.cs
+++ CLAMOSE/Assets/Script/ChargeMachine/Text/BatteryColor.cs
@@ -2,48 +2,64 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BatteryColor : MonoBehaviour {
+public class BatteryColor : MonoBehaviour
+{
 
     //基本的な情報
-    private BatteryCharge batteryCharge;
-    private OverCharge    overchargeState;
-    private short         batteryRate;
-    private Material      material;
-    private Color         chengeColor;
+    public BatteryCharge batteryCharge;        //バッテリー情報
+    public OverCharge overchargeState;      //現在のスマホの状態
+    private short batteryRate;          //バッテリー率を取得して格納する変数
 
-    //緑の大きさについて
-    private 
+    //マテリアル情報
+    private SpriteRenderer nowColor;
+    private Color color;
 
-	// Use this for initialization
-	void Start ()
+    //大きさの情報
+    private float ScaleMax_x;           //最大の横サイズ
+
+
+    // Use this for initialization
+    void Start()
     {
-        this.batteryCharge = GetComponent<BatteryCharge>();
-        this.material = GetComponent<Material>();
-        this.chengeColor = GetComponent<Material>().color;
-        this.overchargeState = GetComponent<OverCharge>();
-	}
-	
-	// Update is called once per frame
-	void Update ()
+        this.color = GetComponent<SpriteRenderer>().color;
+        this.nowColor = GetComponent<SpriteRenderer>();
+        this.nowColor.color = new Color();
+        this.ScaleMax_x = this.gameObject.transform.localScale.x;
+    }
+
+    // Update is called once per frame
+    void Update()
     {
         this.batteryRate = (short)this.batteryCharge.getbatteryRate();
-	}
+        this.nowColor.color = this.CreateBatteryColor();
+        this.transform.localScale = new Vector2(this.GageScale(), this.gameObject.transform.localScale.y);
+    }
 
-    void CreateBatteryColor()
+    Color CreateBatteryColor()
     {
+        Color color = new Color();
         switch (this.overchargeState.getStateName())
         {
             //正常状態
             case "Normal":
-                this.chengeColor = new Color(0, 255, 0);
+                color = Color.green;
                 break;
             case "Adjustment":
+                color = Color.yellow;
                 break;
             case "OverChargeCount":
-                this.chengeColor = new Color(255, 0, 0);
+                color = Color.red;
                 break;
             case "OverCharge":
+                color = Color.red;
                 break;
         }
+        Debug.Log("現在の色　" + color);
+        return color;
+    }
+
+    float GageScale()
+    {
+        return this.ScaleMax_x * this.batteryRate / 100;
     }
 }
