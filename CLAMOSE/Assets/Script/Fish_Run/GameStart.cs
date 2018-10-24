5b01ce3cf1cc3043df8d0cb8ae68c533cc35d170
Tails-Korea-Lumine <saon12@hanmail.net>
2018-10-24 11:52:43 +0900

    2018/10/24
    スタートの追う側、追われる側の説明追加
    勝った方のアニメーション付けが残っている

--- CLAMOSE/Assets/Script/Fish_Run/GameStart.cs
+++ CLAMOSE/Assets/Script/Fish_Run/GameStart.cs
@@ -6,6 +6,7 @@ public class GameStart : MonoBehaviour
 {
     public GameObject bfl, bfr, sfl, sfr;
     public GameObject Goal;
+    public GameObject goal_Marker;
     public GameObject PL, PR;
 
 	// Use this for initialization
@@ -13,7 +14,8 @@ public class GameStart : MonoBehaviour
     {
         int x = Random.Range(0, 10);
 
-        x %= 2;
+        //x %= 2;
+        x = 0;
         //0は右方向に進む
         if (x == 0)
         {
@@ -29,6 +31,8 @@ public class GameStart : MonoBehaviour
             //Instantiate(Goal, goal_Pos, Quaternion.identity);
 
             Goal.transform.position = goal_Pos;
+
+            goal_Marker.transform.position = new Vector3(565, 300, 0);
         }
         //左方向に進む
         else
@@ -36,15 +40,17 @@ public class GameStart : MonoBehaviour
             //各魚の場所に各プレイヤを移動させる
             PL.transform.position = bfl.transform.position;
             PR.transform.position = sfl.transform.position;
-            //左プレイヤは大きい魚            
-            PL.transform.SetParent(Instantiate(bfl).transform);
-            //右プレイヤは小さい魚            
-            PR.transform.SetParent(Instantiate(sfl).transform);
+            //左プレイヤは小さい魚  
+            PL.transform.SetParent(Instantiate(sfl).transform);
+            //右プレイヤは大きい魚            
+            PR.transform.SetParent(Instantiate(bfl).transform);
 
             //ゴール生成
             Vector3 goal_Pos = new Vector3(-3500, 0, 20);
             //Instantiate(Goal,goal_Pos,Quaternion.identity);
             Goal.transform.position = goal_Pos;
+
+            goal_Marker.transform.position = new Vector3(20, 300, 0);
         }
 	}
 	
--- CLAMOSE/Assets/Script/Fish_Run/MoveCamera.cs
+++ CLAMOSE/Assets/Script/Fish_Run/MoveCamera.cs
@@ -10,18 +10,37 @@ public class MoveCamera : MonoBehaviour
 	void Moving ()
     {
         //各プレイヤの位置をとってその中心にカメラが位置する
-        Vector3 lp = left_Player.transform.position;
-        Vector3 rp = right_Player.transform.position;
-
-        Vector3 center = (rp-lp) / 2.0f;
+        Vector3 lp = left_Player.transform.parent.transform.position;
+        Vector3 rp = right_Player.transform.parent.transform.position;
+        Vector3 center;
+        bool is_Going_Right = left_Player.transform.parent.GetComponent<VATfunction>().Get_Angle_X() > 0.0f;
+        if (is_Going_Right)
+        {
+            //右方向のカメラ計算
+            center = (rp - lp) / 2.0f;
+        }
+        else
+        {
+            //左方向カメラ計算
+            center = (lp - rp) / 2.0f;
+        }
        
         //Z位置を計算して適用
         float newz = center.magnitude;
        
-        newz *= root3;       
-        center += lp;        
+        newz *= root3;
+        if (is_Going_Right)
+        {
+            center += lp;
+            center.x += 5;
+        }
+        else
+        {
+            center += rp;
+            center.x -= 5;
+        }
         center.z -= newz ;
-        center.x += 5;
+        
         
         //位置更新
         this.transform.position = center;
