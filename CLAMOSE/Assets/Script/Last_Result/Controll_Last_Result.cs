d7344e558314534a06879cf9f6cb4f382cb9207d
Tails-Korea-Lumine <saon12@hanmail.net>
2018-10-31 19:09:51 +0900

    2018/10/31
    最終リザルトつくり開始、タイムライン制御とタイトルシーンに戻す処理が必要

--- CLAMOSE/Assets/Script/Last_Result/Controll_Last_Result.cs
+++ CLAMOSE/Assets/Script/Last_Result/Controll_Last_Result.cs
@@ -2,15 +2,73 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Controll_Last_Result : MonoBehaviour {
+public class Controll_Last_Result : MonoBehaviour
+{
+    //どっちが勝ったかを保存する場所
+    int[] who_Is_Win;
+    //全体スコア
+    int all_Score;
+
+    //表示するスプライト
+    public GameObject round1, round2, round3,last;
 
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+    {
+        //フラグ初期化
+        who_Is_Win = new int[Game_Selector.max_Games] { -1,-1,-1 };
+
+        //commonデータからもらってくる
+        //今は仮処理
+        for(int i =0; i<Game_Selector.max_Games; i++)
+        {
+            who_Is_Win[i] = 1;
+        }
+
+        //全体スコア計算
+        all_Score = 0;
+        for (int s =0; s<3; s++)
+        {
+            all_Score += who_Is_Win[s];
+        }
+
+        //送る
+        for (int r = 1; r <= 4; r++)
+        {
+            Transport_To_Object(r);
+        }
 	}
+
+    //オブジェクトに勝ち負けを転送するメソッド
+    void Transport_To_Object(int round)
+    {
+        switch(round)
+        {
+            case 1:
+                //転送
+                round1.GetComponent<Controll_Score_Board>().Set_Winner(who_Is_Win[round-1]);
+                break;
+            case 2:
+                //転送
+                round2.GetComponent<Controll_Score_Board>().Set_Winner(who_Is_Win[round-1]);
+                break;
+            case 3:
+                //転送
+                round3.GetComponent<Controll_Score_Board>().Set_Winner(who_Is_Win[round-1]);
+                break;
+            case 4:
+                last.GetComponent<Controll_Score_Board>().Set_Winner(all_Score);
+                break;
+            
+            default:
+                break;
+        }
+    }
+
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 		
 	}
 }
---
+++ CLAMOSE/Assets/Script/Last_Result/Controll_Score_Board.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Controll_Score_Board : MonoBehaviour
+{
+    //このラウンドでだれが勝ったかを保存する場所
+    int win_State;
+    //位置計算のためにラウンド数を保存
+    public int round;
+    //カンバスサイズ
+    float canvas_X, canvas_Y;
+
+    //使うイメージ
+    public Sprite[] last_Result;
+    public Sprite[] round1_Sprite;
+    public Sprite[] round2_Sprite;
+    public Sprite[] round3_Sprite;
+
+    //ステートを保存してもらうメソッド
+    public void Set_Winner(int winner)
+    {
+        if (winner == 4)
+        {
+            win_State = 2;
+        }
+        else
+        {
+            win_State = winner;
+        }
+    }
+    //イメージ選択
+    void Select_Image()
+    {
+        switch(round)
+        {
+            case 1:
+                GetComponent<Image>().sprite = round1_Sprite[win_State];
+                break;
+            case 2:
+                GetComponent<Image>().sprite = round2_Sprite[win_State];
+                break;
+            case 3:
+                GetComponent<Image>().sprite = round3_Sprite[win_State];
+                break;
+            case 4:
+                GetComponent<Image>().sprite = last_Result[win_State];
+                break;
+        }
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+        //カンバスサイズ保存
+        canvas_X = transform.parent.GetComponent<RectTransform>().rect.width;
+        canvas_Y = transform.parent.GetComponent<RectTransform>().rect.height;
+
+        //Y位置計算
+        float y = (canvas_Y / 5.0f) * (5-round);
+
+        //位置更新
+        Vector3 pos = new Vector3(canvas_X / 2.0f, y, 0);
+
+        transform.position = pos;
+
+        Select_Image();
+    }
+
+   
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
